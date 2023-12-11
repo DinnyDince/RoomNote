ee47fba4d50cd801a58dd81586e9c7974a8cace5
Dinny Wahyu Widarti <dinnywidarti@gmail.com>
2023-12-11 09:55:14 +0700

    Buat pertama
    Buat Pertama

---
+++ app/src/main/java/com/dinny/crudroomnotes/room/Note.kt
@@ -0,0 +1,12 @@
+package com.dinny.crudroomnotes.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
